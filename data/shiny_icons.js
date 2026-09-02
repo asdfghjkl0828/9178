198561100331566e3169740174b29a6f42115e71
asdfghjkl0828 <3128926988@qq.com>
2026-09-02 22:55:22 +0800

    新增 data/shiny_icons.js 异色精灵 BWiki 配色图映射

---
+++ data/shiny_icons.js
@@ -0,0 +1,9 @@
+// 异色精灵真实配色图标（来源 BWiki 异色形态图标，仅作配色参考；炫彩随机特效服务器无数据）
+const SHINY_ICONS = {
+  "大耳帽兜": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png",
+  "帽兜娃娃": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png",
+  "菊花梨": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png",
+  "叮叮恶魔": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png",
+  "酷拉": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png",
+  "雪影娃娃": "https://patchwiki.biligame.com/images/rocom/thumb/2/2e/buxc6y4s0r7d8ix03zzkahnk4h8urtv.png/56px-Icon_%E5%BC%82%E8%89%B2_%E5%BD%A9.png"
+};
